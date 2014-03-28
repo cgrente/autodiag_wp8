3abca7e0f60db11bb51d3198687cd4ecd760802c
cgrente <cgrente@gmail.com>
2014-03-28 20:42:17 +0100

    add items

---
+++ Autodiagnostiques/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Autodiagnostiques
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Url of Home page
+        private string MainUri = "http://auto-diagnostic.eu/";//"/Html/index.html";
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+           // Browser.Navigate(new Uri("http://auto-diagnostic.eu/", UriKind.RelativeOrAbsolute));
+        }
+
+        private void Browser_Loaded(object sender, RoutedEventArgs e)
+        {
+            Browser.IsScriptEnabled = true;
+
+            // Add your URL here
+            Browser.Navigate(new Uri(MainUri, UriKind.RelativeOrAbsolute));
+        }
+
+        // Navigates back in the web browser's navigation stack, not the applications.
+        private void BackApplicationBar_Click(object sender, EventArgs e)
+        {
+            Browser.GoBack();
+        }
+
+        // Navigates forward in the web browser's navigation stack, not the applications.
+        private void ForwardApplicationBar_Click(object sender, EventArgs e)
+        {
+            Browser.GoForward();
+        }
+
+        // Navigates to the initial "home" page.
+        private void HomeMenuItem_Click(object sender, EventArgs e)
+        {
+            Browser.Navigate(new Uri(MainUri, UriKind.RelativeOrAbsolute));
+        }
+
+        // Handle navigation failures.
+        private void Browser_NavigationFailed(object sender, System.Windows.Navigation.NavigationFailedEventArgs e)
+        {
+            MessageBox.Show("Navigation to this page failed, check your internet connection");
+        }
+    }
+}
